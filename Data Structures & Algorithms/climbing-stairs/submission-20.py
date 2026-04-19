4a2ee600a79d361bb1bf2c6163333c9635793d64
Idris Popoola <152892043+idris-p@users.noreply.github.com>
2026-04-19 18:57:58 +0100

    Update: climbing-stairs - submission-20

--- Data Structures & Algorithms/climbing-stairs/submission-20.py
+++ Data Structures & Algorithms/climbing-stairs/submission-20.py
@@ -1,14 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        
-        def dp(n, cache={}):
-            if n == 1 or n == 2:
-                return n
-            if n in cache:
-                return cache[n]
+        table = [0] * (n + 1)
+        table[0] = 1
 
-            result = dp(n-2, cache) + dp(n-1, cache)
-            cache[n] = result
-            return result
+        for i in range(0, n):
+            if i + 1 <= n:
+                table[i + 1] += table[i]
+            if i + 2 <= n:
+                table[i + 2] += table[i]
 
-        return dp(n)
+        return table[n]
