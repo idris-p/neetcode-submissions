0d8bc20adad5f02036f4ddcbf920a53f3e1d3674
Idris Popoola <152892043+idris-p@users.noreply.github.com>
2026-05-13 21:34:28 +0100

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        mapping = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"]
+        }
+        result = []
+
+        if not digits:
+            return result
+
+        def explore(i, letters):
+            if len(letters) == len(digits):
+                result.append(letters)
+                return
+            
+            for letter in mapping[digits[i]]:
+                explore(i + 1, letters + letter)
+
+        explore(0, "")
+        return result
