ce9aaf1ccb3740be1e453058f8ea504c32e66a5d
Idris Popoola <152892043+idris-p@users.noreply.github.com>
2026-05-14 20:19:46 +0100

    Add: design-word-search-data-structure - submission-2

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-2.py
@@ -0,0 +1,51 @@
+class TrieNode:
+    def __init__(self, char):
+        self.char = char
+        self.children = {}
+        self.isWord = False
+        
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode("")
+
+    def addWord(self, word: str) -> None:
+        pointer = self.root
+        for letter in word:
+            if letter not in pointer.children:
+                pointer.children[letter] = TrieNode(letter)
+            pointer = pointer.children[letter]
+        pointer.isWord = True
+
+    def search(self, word: str) -> bool:
+        result = False
+        pointer = self.root
+
+        def explore(i, path):
+            nonlocal pointer
+            nonlocal result
+
+            if i == len(word):
+                if pointer.isWord:
+                    result = True
+                return
+            if i != 0 and word[i - 1] != pointer.char and word[i - 1] != ".":
+                return
+
+            temp = pointer
+            if word[i] in pointer.children:
+                pointer = pointer.children[word[i]]
+                explore(i + 1, path + word[i])
+            elif word[i] == ".":
+                for child in pointer.children:
+                    pointer = pointer.children[child]
+                    explore(i + 1, path + word[i])
+                    pointer = temp
+                    if result:
+                        break
+            pointer = temp
+            
+            
+        explore(0, "")
+        return result
